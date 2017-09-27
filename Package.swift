b9bd5ccf12047b315dd3ebdc493c6ddadc88c93e
Norman Sutorius <norman.sutorius@de.ibm.com>
2017-09-27 15:17:25 +0200

    Change the package name

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-  name: "Mustache",
+  name: "MustacheClone",
   dependencies: [
     //TODO make this test dependency once issue https://bugs.swift.org/browse/SR-883 is resolved
       .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 16, minor: 0)
